bc954e01da0f4769cc67c10c4dd25ff9b5803492
Oleksandr Semeniuk <oleksandrsemenuk212@gmail.com>
2023-06-18 21:58:37 +0300

    Injected a dependency into controller

--- Chef_Helper_API/Program.cs
+++ Chef_Helper_API/Program.cs
@@ -3,14 +3,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System;
 using Chef_Helper_API.Controllers;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chef_Helper_API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
-            
+            using (ChefdbContext db = new ChefdbContext()) { 
+                db.Database.EnsureCreated();
+                db.Database.CanConnect();
+            }
         }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Добавляем DbContext в DI-контейнер
+                services.AddDbContext<ChefdbContext>(options =>
+                    options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
+
+                // Добавляем контроллеры в DI-контейнер
+                services.AddControllers();
+            });
     }
 }
