44f9dc391184f0177b8c16b56421509b7610c756
Andrey <lem4aks@gmail.com>
2023-06-26 11:06:52 +0300

    fix js

--- Chef_Helper_Web/wwwroot/js/site.js
+++ Chef_Helper_Web/wwwroot/js/site.js
@@ -38,5 +38,39 @@ function getDataing() {
 }
 var addrecpt = document.getElementById("addrecpt");
 addrecpt.addEventListener("click", getDatarecpt);
-var dellrecpt = document.getElementById("dellrecpt");
-dellrecpt.addEventListener("click", getDatarecpt);
+function getDatarecpt() {
+    var receptName = document.getElementById("receptNameInput").value;
+    var needQuantity = document.getElementById("needQuantityInput").value;
+    var calories = document.getElementById("caloriesInput").value;
+    var Weight = document.getElementById("WeightInput").value;
+
+    var data = {
+        receptName: receptName,
+        needQuantity: needQuantity,
+        calories: calories,
+        Weight: Weight
+    };
+
+    fetch('api/Recipe', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Failed to add warehouse');
+            }
+        })
+        .then(result => {
+            // Обработка успешного ответа от сервера
+            console.log(result);
+        })
+        .catch(error => {
+            // Обработка ошибок
+            console.error(error);
+        });
+}
